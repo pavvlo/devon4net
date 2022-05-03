60d078b4172d3333c6cd4d75cd959938dd0dd691
pavvlo <pbmocholi_98@hotmail.es>
2022-05-03 10:32:05 +0200

    Added Equals method to Employee for testing sample

--- source/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Domain/Entities/Employee.cs
+++ source/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Domain/Entities/Employee.cs
@@ -24,5 +24,14 @@
         /// mail
         /// </summary>
         public string Mail { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(Employee)) return false;
+            Employee expected = obj as Employee;
+            return expected.Name == Name
+                && expected.Surname == Surname 
+                && expected.Mail == Mail;
+        }
     }
 }
